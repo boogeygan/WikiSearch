8a51a72cd7f7000ded68f25ba1f591e5dc461210
Gagandeep Chhabra <chhabragdeep@gmail.com>
2015-07-24 23:05:44 +0530

    Create FieldType.java

---
+++ src/FieldType.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dom.gun.ire.minor;
+
+/**
+ *
+ * @author gagan
+ */
+public enum FieldType {
+	title, id, content,infobox, outlinks, categories, noval
+}
